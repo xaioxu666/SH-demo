77cceac66e554e268c2fb789dbbb8d1e0c201f1e
xiaoxufa <1738241690@qq.com>
2021-08-30 23:11:28 +0800

    新建了app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+app.get('/xu', (req, res) => {
+
+  res.send('hello from simple server :)')
+
+})
+app.listen(3000, () => {
+  console.log('30eer00rgt');
+})
